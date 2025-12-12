1b39c99207443a514af8ac072c6e50376a675604
kingprithvi01-star <kingprithvi01@gmail.com>
2025-12-12 15:51:19 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,3 @@
+import React from 'react';
+import { momsbelief, icegram, trackier, rentcity, visa, igaming, vansha } from './projectImages';
+export default function App(){return (<div>Portfolio Loaded</div>);}
