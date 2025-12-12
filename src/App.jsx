41f5828eea442182538a4c429eeaa8c6cb82c445
kingprithvi01-star <kingprithvi01@gmail.com>
2025-12-12 15:44:46 +0530

    Delete src directory

--- src/App.jsx
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { momsbelief, icegram, trackier, rentcity, visa, igaming, vansha } from './projectImages';
-export default function App(){return (<div>Portfolio Loaded</div>);}
